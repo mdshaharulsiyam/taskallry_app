73bd4659077848257e2884941e65e138808d1387
shaharulsiyam <shaharulsiyam0273@gmail.com>
2025-11-25 14:08:13 +0600

    chat infinity paginate done with loader

--- src/screens/tabs/Chat.tsx
+++ src/screens/tabs/Chat.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { ActivityIndicator, FlatList, StyleSheet } from "react-native";
 import ChatItems from "../../components/chat/ChatItems";
 import SafeAreaProviderNoScroll from "../../providers/SafeAreaProviderNoScroll";
 import { useGetChatListQuery } from "../../redux/apis/conversationApi";
 
 const Chat = () => {
-  const { data, isLoading } = useGetChatListQuery({
+  const [limit, setLimit] = useState(20);
+
+  const { data, isLoading, isFetching } = useGetChatListQuery({
     page: 1,
-    limit: 10,
+    limit,
   });
+
+  const list = data?.data?.data || [];
   return (
     <SafeAreaProviderNoScroll>
       {isLoading ? (
@@ -20,7 +24,19 @@ const Chat = () => {
             paddingBottom: 150,
           }}
           showsVerticalScrollIndicator={false}
-          data={data?.data?.data || []}
+          data={list}
+          onEndReachedThreshold={0.1}
+          onEndReached={() => {
+            const total = data?.data?.meta?.total || 0;
+            if (!isFetching && total > list.length) {
+              setLimit((prev) => prev + 20);
+            }
+          }}
+          ListFooterComponent={
+            isFetching && list.length > 0 ? (
+              <ActivityIndicator style={{ marginVertical: 8 }} />
+            ) : null
+          }
           renderItem={({ item }) => <ChatItems item={item} />}
         />
       )}

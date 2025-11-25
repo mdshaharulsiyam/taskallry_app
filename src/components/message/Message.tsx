41730863423d76cf853d0c6c50a7e313e64c82e9
shaharulsiyam <shaharulsiyam0273@gmail.com>
2025-11-25 13:43:15 +0600

    image shiw oerfectly now

--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, TextStyle } from "react-native";
+import { Image, StyleSheet, TextStyle, View } from "react-native";
 import { MessageItem } from "../../redux/apis/messageApi";
 import TextPrimary from "../shered/TextPrimary";
 
@@ -8,17 +8,41 @@ const Message = ({ item }: { item: MessageItem }) => {
     ? { alignSelf: "flex-end", backgroundColor: "#FFFFFF" }
     : { alignSelf: "flex-start", backgroundColor: "#E6F4F1" };
 
+  const hasText = !!item.text && item.text.trim().length > 0;
+  const hasImage = Array.isArray(item.imageUrl) && item.imageUrl.length > 0;
+
   return (
-    <TextPrimary
-      text={item.text}
+    <View
       style={{
         ...alignStyle,
         borderRadius: 10,
-        paddingHorizontal: 12,
-        paddingVertical: 8,
+        paddingHorizontal: 8,
+        paddingVertical: 6,
         maxWidth: "80%",
       }}
-    />
+    >
+      {hasImage && (
+        <Image
+          source={{ uri: item.imageUrl[0] }}
+          style={{
+            width: 180,
+            height: 180,
+            borderRadius: 8,
+            marginBottom: hasText ? 6 : 0,
+          }}
+          resizeMode="cover"
+        />
+      )}
+      {hasText && (
+        <TextPrimary
+          text={item.text}
+          style={{
+            fontSize: 14,
+            color: "#111827",
+          }}
+        />
+      )}
+    </View>
   );
 };
 
